e9a85777bfbdb994c9e3cec44ec761e37f317eec
davidbecks85-spec <davidbecks85@gmail.com>
2026-08-10 10:23:06 +0200

    Create main.py

---
+++ .github/main.py
@@ -0,0 +1,19 @@
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+from kivymd.uix.screen import MDScreen
+
+class SatTrackerApp(MDApp):
+    def build(self):
+        self.theme_cls.primary_palette = "Blue"
+        screen = MDScreen()
+        
+        label = MDLabel(
+            text="SatTracker Pro - Pronto!",
+            halign="center",
+            font_style="H4"
+        )
+        screen.add_widget(label)
+        return screen
+
+if __name__ == "__main__":
+    SatTrackerApp().run()
